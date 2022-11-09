c3731a29535f41a7fcb4e4b2803d9a27bc58da86
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-09 23:09:44 +0000

    GUI almost ready, next task is to work on the label widget with a scroll.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import wolframalpha
 import wikipedia
 from tkinter import *
@@ -15,25 +16,62 @@ def user_input():
         result = client.query(search)
         answer = next(result.results).text
         print(answer)
+        textarea.config(text="" + answer)
+
     except:
         # wikipedia
         print(wikipedia.summary(search))
 
 
-input_ent = Entry(root, width=57)
-input_ent.place(x=26, y=200)
-
-submit_btn = Button(root, width=15, height=2, text="Submit", command=user_input)
-submit_btn.place(x=50, y=230)
-
-clear_btn = Button(root, width=15, height=2, text="Clear")
-clear_btn.place(x=230, y=230)
-
-result_bar = Scrollbar(root, width=360, orient=VERTICAL)
-result_bar.place(x=20, y=270)
+def clear_search():
+    clear = input_ent.delete(0, "end")
 
 
-root.geometry("400x550")
+input_ent = Entry(root, 
+                  width=57)
+input_ent.place(x=26, y=100)
+
+submit_btn = Button(root, 
+                    width=22, 
+                    height=2, 
+                    text="Submit",
+                    command=user_input)
+submit_btn.place(x=25, y=130)
+
+clear_btn = Button(root, 
+                   width=22, 
+                   height=2, 
+                   text="Clear",
+                   command=clear_search)
+clear_btn.place(x=208, y=130)
+
+'''
+
+needs fixing
+
+result_lab = Label(root,
+                   width=49,
+                   height=10,
+                   bg="grey")
+result_lab.place(x=25, y=190)
+
+v_s = Scrollbar(result_lab)
+v_s.pack(side=RIGHT, fill=Y)
+
+textarea = Label(root,
+                width=43,
+                height=5,
+                wrap=NONE,
+                yscrollcommand=v_s.set)
+textarea.place(x=20, y=270)
+
+root.geometry("400x400")
 root.resizable(0, 0)
 root.attributes("-topmost", True)
 root.mainloop()
+
+https://www.youtube.com/watch?v=BckVJoE94Lk
+https://github.com/Azzy001/Vehicle_Management_System_Tk/blob/main/Sales_GUI.py
+https://github.com/Azzy001/Contact_Book/blob/main/main_contact.py
+https://github.com/Azzy001/Guess_Game_Tk/blob/main/guess_game_Tk.py
+'''
