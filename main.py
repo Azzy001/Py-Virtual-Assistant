0c951a094214aef2a549f8f4136276a8ce734ae2
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-09 22:05:35 +0000

    added tkinter widgets to my program, will add a scrollbar that will return the search results.

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import wolframalpha
 import wikipedia
+from tkinter import *
 
-while True:
-    user = input("Question: ")
+root = Tk()
 
+
+def user_input():
+    search = (input_ent.get())
+    
     try:
         # wolframalpha
-        api_id = "******-**********"
+        api_id = "******-**********" # you will need to enter the API key from wolframalpha
         client = wolframalpha.Client(api_id)
-        result = client.query(user)
+        result = client.query(search)
         answer = next(result.results).text
         print(answer)
     except:
         # wikipedia
-        print(wikipedia.summary(user))
+        print(wikipedia.summary(search))
+
+
+input_ent = Entry(root, width=57)
+input_ent.place(x=26, y=200)
+
+submit_btn = Button(root, width=15, height=2, text="Submit", command=user_input)
+submit_btn.place(x=50, y=230)
+
+clear_btn = Button(root, width=15, height=2, text="Clear")
+clear_btn.place(x=230, y=230)
+
+result_bar = Scrollbar(root, width=360, orient=VERTICAL)
+result_bar.place(x=20, y=270)
+
+
+root.geometry("400x550")
+root.resizable(0, 0)
+root.attributes("-topmost", True)
+root.mainloop()
