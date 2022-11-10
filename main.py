46d5011ae99218056f00b29f35fe5d2c8886fb4e
Azzy <Muhammadazzy@hotmail.co.uk>
2022-11-10 07:00:49 +0000

    Code completed.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from unittest import result
 import wolframalpha
 import wikipedia
 from tkinter import *
@@ -7,30 +8,35 @@ root = Tk()
 
 
 def user_input():
+    """ """
     search = (input_ent.get())
     
     try:
         # wolframalpha
-        api_id = "******-**********" # you will need to enter the API key from wolframalpha
+        api_id = "******-**********" # <-- you will need to enter the API key from wolframalpha
         client = wolframalpha.Client(api_id)
         result = client.query(search)
         answer = next(result.results).text
         print(answer)
-        textarea.config(text="" + answer)
-
+        result_text.insert(END, answer)
+    
     except:
-        # wikipedia
+        # returning a summary from wiki for your question
         print(wikipedia.summary(search))
 
-
+  
 def clear_search():
-    clear = input_ent.delete(0, "end")
+    """This function is used to clear the values from search entry and from the results box"""
+    input_ent.delete(0, "end")
+    result_text.delete(1.0, END)
+   
 
-
+# input box for search
 input_ent = Entry(root, 
                   width=57)
 input_ent.place(x=26, y=100)
 
+# submit button
 submit_btn = Button(root, 
                     width=22, 
                     height=2, 
@@ -38,6 +44,7 @@ submit_btn = Button(root,
                     command=user_input)
 submit_btn.place(x=25, y=130)
 
+# button to remove search and results
 clear_btn = Button(root, 
                    width=22, 
                    height=2, 
@@ -45,33 +52,13 @@ clear_btn = Button(root,
                    command=clear_search)
 clear_btn.place(x=208, y=130)
 
-'''
-
-needs fixing
-
-result_lab = Label(root,
-                   width=49,
-                   height=10,
-                   bg="grey")
-result_lab.place(x=25, y=190)
-
-v_s = Scrollbar(result_lab)
-v_s.pack(side=RIGHT, fill=Y)
-
-textarea = Label(root,
-                width=43,
-                height=5,
-                wrap=NONE,
-                yscrollcommand=v_s.set)
-textarea.place(x=20, y=270)
+# text box for displaying search results
+result_text = Text(root,
+                   width=43,
+                   height=10)
+result_text.place(x=25, y=190)
 
 root.geometry("400x400")
 root.resizable(0, 0)
 root.attributes("-topmost", True)
 root.mainloop()
-
-https://www.youtube.com/watch?v=BckVJoE94Lk
-https://github.com/Azzy001/Vehicle_Management_System_Tk/blob/main/Sales_GUI.py
-https://github.com/Azzy001/Contact_Book/blob/main/main_contact.py
-https://github.com/Azzy001/Guess_Game_Tk/blob/main/guess_game_Tk.py
-'''
